2a19605932e1d1f89b10eaadc646261426b02da2
mike <chennanbang@gmail.com>
2017-08-17 14:32:26 +0800

    用程式增加一些按鈕，並增加之間的空間

--- CloudsAPP/RatingControl.swift
+++ CloudsAPP/RatingControl.swift
@@ -10,22 +10,51 @@ import UIKit
 
 class RatingControl: UIStackView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    private var starButtons = [UIButton]()
+    var stars = 0
     
     override init(frame: CGRect) {
         
         super.init(frame: frame)
+        
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         
         super.init(coder: coder)
+        
+        setupButtons()
     }
 
+    private func setupButtons() {
+        //產生 1 個 button
+//        let button = UIButton()
+//        button.backgroundColor = UIColor.black
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+//        button.widthAnchor.constraint(equalToConstant: 20.0).isActive = true
+//        
+//        button.addTarget(self, action: #selector(RatingControl.tapButton(button:)), for: .touchUpInside)
+//        
+//        addArrangedSubview(button)
+        
+        //產生一些 button
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.black
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 20.0).isActive = true
+            
+            button.addTarget(self, action: #selector(RatingControl.tapButton(button:)), for: .touchUpInside)
+            
+            addArrangedSubview(button)
+            starButtons.append(button)
+        }
+    }
+    
+    func tapButton(button: UIButton) {
+        print("點評")
+    }
 }
