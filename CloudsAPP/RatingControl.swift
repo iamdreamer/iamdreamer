a2d5d7b9e538936b7bf1228ae5d8addc5a8b1870
mike <chennanbang@gmail.com>
2017-08-17 14:03:35 +0800

    新增點評用的 UI 跟檔案

---
+++ CloudsAPP/RatingControl.swift
@@ -0,0 +1,31 @@
+//
+//  RatingControl.swift
+//  CloudsAPP
+//
+//  Created by mike on 2017/8/17.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        
+        super.init(coder: coder)
+    }
+
+}
